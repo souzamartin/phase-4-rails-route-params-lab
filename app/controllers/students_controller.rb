938e0de40aaf80c4cdababb28064a943c5d0f42b
Martin <souzam@sonic.net>
2022-12-12 10:19:47 -0500

    commit tree fa87a76eb6a434b9d1336deee274090a5c17218f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
+    # if params[:first_name]
+    #   render json: Student.all.filter {|student| student.to_s.include?(params[:first_name])}
+    # elsif params[:last_name]
+    #   render json: Student.all.filter {|student| student.to_s.include?(params[:last_name])}
+    # else
     students = Student.all
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
